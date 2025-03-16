73a966de6e803f3fa61e296d2547f351b7fb64df
Emad Abaei <emad.ae14@gmail.com>
2025-03-16 12:47:57 +0330

    fix(CreateUser): add button type

--- src/features/user/CreateUser.tsx
+++ src/features/user/CreateUser.tsx
@@ -40,7 +40,7 @@ function CreateUser() {
 
       {username !== "" && (
         <div>
-          <Button>Start ordering</Button>
+          <Button type='submit'>Start ordering</Button>
         </div>
       )}
     </form>
