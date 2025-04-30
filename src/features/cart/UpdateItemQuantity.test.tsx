e2f8db1029329b82cd886e9e629f2eea4816a33e
Emad Abaei <emad.ae14@gmail.com>
2025-04-30 11:11:14 +0330

    feat: add UpdateItemQuantity.test.tsx

---
+++ src/features/cart/UpdateItemQuantity.test.tsx
@@ -0,0 +1,32 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { useDispatch } from "react-redux";
+import UpdateItemQuantity from "./UpdateItemQuantity";
+
+// Mock the Redux dispatch function
+const mockDispatch = vi.fn();
+vi.mock("react-redux", async () => {
+  const actual = await vi.importActual("react-redux");
+  return {
+    ...actual,
+    useDispatch: () => mockDispatch
+  };
+});
+
+describe("UpdateItemQuantity", () => {
+  it("dispatches increase and decrease actions when buttons are clicked", () => {
+    render(<UpdateItemQuantity pizzaId={1} currentQuantity={2} />);
+
+    const decreaseButton = screen.getByRole("button", {
+      name: /decrease quantity of pizza 1/i
+    });
+    const increaseButton = screen.getByRole("button", {
+      name: /increase quantity of pizza 1/i
+    });
+
+    fireEvent.click(decreaseButton);
+    fireEvent.click(increaseButton);
+
+    expect(mockDispatch).toHaveBeenCalledTimes(2);
+  });
+});
